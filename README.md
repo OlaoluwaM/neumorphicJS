c197f6bc5c4c6dbeb55f36d6ec7c21a0b2ac1a27
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2020-12-31 10:58:01 -0600

    Some housekeeping

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-#put stuff
+<h1>Neumorphic JS</h1>
+
+<p>A small module within which I tried to make adding neumorphic effects to elements more programmatic and declarative.</p>
+<p>The functionality is reverse engineered from this <a href="https://neumorphism.io/#55b9f3">wonderful web application</a> that helps generate the code for the neumorphic effect</p>
+
+<p>Built with Javascript. Tested with Jest</p>
+<p>I built this as a kind of test, to see if I could reverse engineer Neumorphism.io for the code required to generate the effect. Therefore, it is not a complete module</p>
+<p>Oh, and in case you were wondering, I was successful. You can check by cloning this and running one of the test scripts</p>
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import { generateDistances, propertyChecker, rgbaToHex } from "./helpers";
+import { generateDistances, propertyChecker, rgbaToHex } from './helpers';
 
-const shapes = ["concave", "flat", "convex", "pressed"];
+const shapes = ['concave', 'flat', 'convex', 'pressed'];
 
 const lightAngles = {
-  topLeft: "145deg",
-  topRight: "225deg",
-  bottomRight: "315deg",
-  bottomLeft: "45deg",
+  topLeft: '145deg',
+  topRight: '225deg',
+  bottomRight: '315deg',
+  bottomLeft: '45deg',
 };
 export const defaults = {
-  shape: "flat",
-  color: "#55b9f3",
+  shape: 'flat',
+  color: '#55b9f3',
   intensity: 0.15,
-  lightSource: "topLeft",
+  lightSource: 'topLeft',
   blur: 60,
   distance: 20,
 };
@@ -42,25 +42,25 @@ export function deriveOptions(userOptions) {
  */
 export function colorDifference(color, step = 0) {
   try {
-    if (Number.isInteger(step)) throw "Intensity value can only be a float";
+    if (Number.isInteger(step)) throw 'Intensity value can only be a float';
 
     if (step > 0.6 || step < -0.6) {
-      throw "Value of intensity property must be < 0.6 or > -0.6";
+      throw 'Value of intensity property must be < 0.6 or > -0.6';
     }
 
-    const pattern = new RegExp(/[\d\w]{2}/, "g");
+    const pattern = new RegExp(/[\d\w]{2}/, 'g');
     const localColorValue = rgbaToHex(color);
-    let modifiedHex = "#";
+    let modifiedHex = '#';
 
-    localColorValue.match(pattern).forEach((hexGroup) => {
+    localColorValue.match(pattern).forEach(hexGroup => {
       const asDecimal = parseInt(hexGroup, 16);
       let tempVar;
 
-      modifiedHex += "00"
+      modifiedHex += '00'
         .concat(
-          (tempVar = Math.round(
-            Math.min(Math.max(0, asDecimal + asDecimal * step), 255)
-          ).toString(16))
+          (tempVar = Math.round(Math.min(Math.max(0, asDecimal + asDecimal * step), 255)).toString(
+            16
+          ))
         )
         .substr(tempVar.length);
     });
@@ -85,15 +85,15 @@ export function neumorph(optionObj = defaults) {
   const { distance, color, shape, blur, intensity, lightSource } = userOptions;
   const distances = generateDistances(distance, lightSource);
 
-  const inset = shape === shapes[3] ? " inset" : "";
+  const inset = shape === shapes[3] ? ' inset' : '';
   let gradient = `background: ${color};`;
 
   const darkerColor = colorDifference(color, -intensity);
   const lighterColor = colorDifference(color, intensity);
 
-  if (shapes.includes(shape) && shape.includes("con")) {
+  if (shapes.includes(shape) && shape.includes('con')) {
     const angle = lightAngles[lightSource];
-    const isConcave = shape.endsWith("cave");
+    const isConcave = shape.endsWith('cave');
 
     const color1 = colorDifference(color, isConcave ? 0.07 : -0.1);
     const color2 = colorDifference(color, isConcave ? -0.1 : 0.07);
@@ -103,8 +103,8 @@ export function neumorph(optionObj = defaults) {
 
   return [
     gradient,
-    "\n",
+    '\n',
     `box-shadow:${inset} ${distances[0]}px ${distances[1]}px ${blur}px ${darkerColor},`,
     `${inset} ${distances[2]}px ${distances[3]}px ${blur}px ${lighterColor};`,
-  ].join("");
+  ].join('');
 }
